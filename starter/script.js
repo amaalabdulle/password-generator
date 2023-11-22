b50e7808905ec6c31bb0673781b48d2f2b76167f
amaal abdulle <amaalabdelmuid@gmail.com>
2023-11-22 17:20:26 +0000

    random element from array function finished

--- starter/script.js
+++ starter/script.js
@@ -22,11 +22,11 @@ var specialCharacters = [
   '~',
   '-',
   '_',
-  '.'
+  '.' // 23
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // 10
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
@@ -55,7 +55,7 @@ var lowerCasedCharacters = [
   'w',
   'x',
   'y',
-  'z'
+  'z' //26
 ];
 
 // Array of uppercase characters to be included in password
@@ -85,36 +85,68 @@ var upperCasedCharacters = [
   'W',
   'X',
   'Y',
-  'Z'
+  'Z' // 26
 ];
 
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-// length between 8 and 128.
-  var length = prompt ("enter a password with a length of minimum 8 characters but no more than 128.");
+  // length between 8 and 128.
+
+  var length = prompt ("Enter a password with a length of minimum 8 characters but no more than 128.");
+  
   if (length < 8 || length > 128) {
-    alert("the password you have entered does not meet the length requirements");
-  } else () {
-    
+    alert ("The password you have entered does not meet the length requirements");
+  } else {
+    alert ("The password you have entered meets the length requirements") 
   }
-// special characters
+
+  // checking if special characters have been included
   var specialCharacters = confirm ("Does your password contain a special character?");
   var numericCharacters  = confirm ("Does your password contain a numeric character?");
   var lowerCasedCharacters = confirm ("Does your password contain a lower cased character?");
   var upperCasedCharacters = confirm ("Does your password contain an upper cased character?");
-}
 
+  // && checks that boolean operator is true.
+  // to check if it is false add exclamation mark in front of it.
+  if (!specialCharacters && !numericCharacters && !lowerCasedCharacters && !upperCasedCharacters)
+    alert ("Please enter at lease one special character!");
+    return;
+}
+var options = getPasswordOptions();
+console.log(options);
 
 // Function for getting a random element from an array
-function getRandom(arr) {
 
+function getRandomSpecial(specialCharacters) {
+  return specialCharacters (Math.floor(Math.random() * 23)); // Math.floor() // rounded down
 }
+console.log(getRandomSpecial);
+
+function getRandomNumeric(numericCharacters) {
+  return numericCharacters (Math.floor(Math.random() * 10));
+}
+console.log(getRandomNumeric);
+
+function getRandomLower(lowerCasedCharacters) {
+  return lowerCasedCharacters (Math.floor(Math.random() * 26));
+}
+console.log(getRandomLower);
+
+function getRandomUpper(upperCasedCharacters) {
+  return upperCasedCharacters (Math.floor(Math.random() * 26));
+}
+console.log(getRandomUpper);
+
 
 // Function to generate password with user input
-function generatePassword() {
-
-}
+// function generatePassword() {
+//   var password = "";
+//   for (var i = 0; i < length; i++) {
+//     password = password + 
+//   }
+  
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
